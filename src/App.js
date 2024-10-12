e51f4b0be099bc06ad4f3d3acf8ea841ff14730e
MahdiBenRejeb <mahdi.benrejeb@esprit.tn>
2024-10-12 16:09:51 +0100

    frontend first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get('http://localhost:8081/api/products'); // Back-end API URL
+                setProducts(response.data);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des produits', error);
+            }
+        };
+        fetchProducts();
+    }, []);
+
+    return (
+        <div className="container mt-5">
+            <h1 className="text-center mb-4">Liste des Produits</h1>
+            <ul className="list-group">
+                {products.map(product => (
+                    <li key={product.id} className="list-group-item">
+                        <div className="d-flex justify-content-between">
+                            <div>
+                                <h5>{product.name}</h5>
+                                <p className="mb-1">Prix: {product.price} €</p>
+                                <p className="mb-1">Quantité: {product.quantity}</p>
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
+
+
